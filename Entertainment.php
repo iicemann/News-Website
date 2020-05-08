17c3f55f0c6d71d479f4de0dd756ed67d24d472e
Abhishek Anjan Pai <64316558+iicemann@users.noreply.github.com>
2020-05-08 21:12:36 +0530

    Create Entertainment.php

---
+++ Entertainment.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+ <title>Crisp Entertainment</title>
+</head>
+
+<style>
+ul {
+  float: left;
+  list-style-type: none;
+  margin-top: 155px;
+}
+
+ul li{
+  display: inline-block;
+}
+
+ul li a{
+  text-decoration: none;
+  color: #00FF00;
+  padding: 5px 20px;
+  border: 1px solid transparent;
+  transition: 0.6s ease;
+  font-family: "montserrat";
+}
+
+ul li a:hover{
+  background-color: #FFA500;
+  color: #00FFFF;
+}
