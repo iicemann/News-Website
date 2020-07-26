69547e26bb00246f953646b2047c19f3161c88b4
Abhishek Anjan Pai <64316558+iicemann@users.noreply.github.com>
2020-07-26 12:20:03 +0530

    Create subscribe.php

---
+++ subscribe.php
@@ -0,0 +1,26 @@
+<?php
+	//$EMail = $_POST['username'];
+	$EMail = $_POST['email'];
+	$Genre = $_POST['genre'];
+	$Period = $_POST['period'];
+	session_start();	
+
+	$conn = new mysqli('localhost','root','','admin');
+	if($conn->connect_error)
+	{ 
+		die('Connection Failed : '.$conn->connect_error);
+	}
+	else
+	{
+		$stmt = $conn->prepare("insert into add_subscriber(User_Id, Email_Id, Edition, Period)
+			values(?, ?, ?, ?)");
+		$stmt->bind_param("issi",$_SESSION['uid'], $EMail, $Genre, $Period);
+		$stmt->execute();
+		//echo $_SESSION['uid'];
+		$reg = "select * from add_subscriber where User_Id='".$_SESSION['uid']."' and Email_Id ='$EMail' and Edition='$Genre' and  Period='$Period' ";
+		$_SESSION['sid']=$row['Subscribe_Id'];
+		header("Location: display_bill.php");
+		$stmt->close();
+		$conn->close();
+	}
+?>
